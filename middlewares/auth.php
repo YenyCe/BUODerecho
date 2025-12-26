48c4048e50a5718c415012c1d05117628779ecd8
YenyCe <95324297+YenyCe@users.noreply.github.com>
2025-12-26 10:43:30 -0600

    cambios

--- middlewares/auth.php
+++ middlewares/auth.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-if(!isset($_SESSION['id_usuario'])){
-    header("Location: login.php");
+
+if (!isset($_SESSION['id_usuario'])) {
+    header("Location: /views/login.php");
     exit();
 }

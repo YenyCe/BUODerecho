696f3a0755544e276c70074de533f4d3cbfcd8c4
YenyCe <95324297+YenyCe@users.noreply.github.com>
2025-12-26 10:29:52 -0600

    cambio bd

--- config/conexion.php
+++ config/conexion.php
@@ -1,12 +1,13 @@
 <?php
 $host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "escuela";
+$user = "u224139272_buoadmin";
+$pass = "BUOlistas2026";
+$dbname = "u224139272_buolistas";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
 if($conn->connect_error){
     die("Error de conexión: " . $conn->connect_error);
 }
+$conn->set_charset("utf8mb4");
 ?>

5df0286859bd7d951678f53459e46fd7bd77271a
YenyCe <95324297+YenyCe@users.noreply.github.com>
2025-12-26 10:49:48 -0600

    add

--- views/dashboard.php
+++ views/dashboard.php
@@ -9,7 +9,7 @@ $pagina = $pagina ?? 'dashboard'; // Si la página no define $pagina, usar 'dash
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?? "Sistema Académico"; ?></title>
-    <link rel="stylesheet" href="/ASISTENCIAS/css/styles.css">
+    <link rel="stylesheet" href="/css/styles.css">
 </head>
 
 <body>

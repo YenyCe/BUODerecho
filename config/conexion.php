cc4db77a0afd5ec379d51cf94ac27caa1a499986
YenyCe <95324297+YenyCe@users.noreply.github.com>
2026-01-08 20:10:57 -0600

    Update conexion.php

--- config/conexion.php
+++ config/conexion.php
@@ -1,9 +1,7 @@
-
-
 <?php
 $host = "localhost";
-$user = "root";
-$pass = "";
+$user = "u224139272_buoadmin";
+$pass = "BUOlistas2026";
 $dbname = "u224139272_buolistas";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
@@ -11,4 +9,5 @@ $conn = new mysqli($host, $user, $pass, $dbname);
 if($conn->connect_error){
     die("Error de conexión: " . $conn->connect_error);
 }
+$conn->set_charset("utf8mb4");
 ?>
